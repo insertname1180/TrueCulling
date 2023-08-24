526979e67d0ac00cc00d3ae051ad2ba0245a2591
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-24 18:08:33 -0400

    Update TrueCulling.lua

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -1,36 +1,36 @@
 if CLIENT then
 
-    CULLFRONT = math.sin(0.42261826)
-    jit.on()
+	CULLFRONT = math.sin(0.42261826)
+	jit.on()
 
-    hook.Add( "InitPostEntity", "init", init)
+	hook.Add( "InitPostEntity", "init", init)
 
-    function init()
-        ply = LocalPlayer()
-    end
+	function init()
+		ply = LocalPlayer()
+	end
 
-    hook.Remove("InitPostEntity", "init")
+	hook.Remove("InitPostEntity", "init")
 
-    function main() -- more optimized than think        
-        init()    
-        tAllEnts = ents.GetAll()
-        for a, e in ipairs(tAllEnts) do
-            e:SetNoDraw(true)
-            if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then
-                if ply:IsLineOfSightClear(e:GetPos()) then
-                    e:SetNoDraw(false)
-                end
-            end
-            if e:IsPlayer() then
-                e:SetNoDraw(false)
-            end
-            if e:GetClass() == "viewmodel" and "weapon" and "info_node" then
-                e:SetNoDraw(false)
-            end
-        end
-        timer.Simple(0.2, main)
-    end -- End of main
+	function main() -- more optimized than think		
+		init()	
+		tAllEnts = ents.GetAll()
+		for a, e in ipairs(tAllEnts) do
+			e:SetNoDraw(true)
+			if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then
+				if ply:IsLineOfSightClear(e:GetPos()) then
+					e:SetNoDraw(false)
+				end
+			end
+			if e:IsPlayer() then
+				e:SetNoDraw(false)
+			end
+			if e:GetClass() == "viewmodel" and "weapon" and "info_node" then
+				e:SetNoDraw(false)
+			end
+		end
+		timer.Simple(0.2, main)
+	end -- End of main
 
-    main()
+	main()
 
 end
