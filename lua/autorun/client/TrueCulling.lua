41cfe1d3335c16b10ec6f9a5faa82ee809777a50
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-24 12:23:12 -0400

    Same as workshop code

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -4,15 +4,18 @@ function runonce()
 
 		--Initialize run-once variables
 
-		PROPS_AND_NPCS_ONLY = true
 		CULLFRONT = math.sin(0.42261826) -- 150 degrees forward
 		ply = LocalPlayer()
 		jit.on()
 
+		-- Remove hook as we no longer need it
+		hook.Remove("InitPostEntity", "init")		
+			
 		--Initialize functions
-		function main() -- more optimized than think
-			tAllEnts = ents.GetAll()		
-			for a, e in pairs(tAllEnts) do
+		
+		function main() -- more optimized than think		
+			tAllEnts = ents.GetAll()
+			for a, e in ipairs(tAllEnts) do
 				e:SetNoDraw(true)
 				if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then
 					e:SetNoDraw(true)
@@ -23,13 +26,16 @@ function runonce()
 				if e:IsPlayer() then
 					e:SetNoDraw(false)						
 				end
-				if e:GetClass() == "viewmodel" and "player" and "weapon" and "info_node" then
+				if e:GetClass() == "viewmodel" and "weapon" and "info_node" then
 					e:SetNoDraw(false)
 				end
 			end
 		end
 
-		timer.Create("pootis", 0.2, 0, main)
+		-- Initialize coroutines
+		coroutine.create(main)
+		
+		timer.Create("pootis", 0.25, 0, main)
 
 	end -- End of if
 
