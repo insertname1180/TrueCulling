ee484c8e1d204635089a40e0e0f74b3c2c75a1a8
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-24 06:27:45 -0400

    Refactor TrueLOD and reduce checks

--- lua/autorun/client/TrueLOD.lua
+++ lua/autorun/client/TrueLOD.lua
@@ -3,42 +3,34 @@ function runonce()
 	if CLIENT then
 
 		--Initialize run-once variables
+
 		PROPS_AND_NPCS_ONLY = true
-		CULLFRONT = math.sin(0) --180deg
-		ply = LocalPlayer()		
+		CULLFRONT = math.sin(0.42261826) -- 150 degrees forward
+		ply = LocalPlayer()
 		jit.on()
 
 		--Initialize functions
-		
 		function main() -- more optimized than think
-			tAllEnts = ents.GetAll()
-			for a, e in ipairs(tAllEnts) do
-				vEntPos = e:GetPos() -- Get its position...
-				vPlyDir = ply:GetAimVector()
-				vPlyPos = ply:GetPos()				
+			tAllEnts = ents.GetAll()		
+			for a, e in pairs(tAllEnts) do
 				e:SetNoDraw(true)
-				if e:GetClass() == "viewmodel" and "player" and "weapon" and "info_node" then
-					e:SetNoDraw(false)
-				end
-				if util.IsPointInCone(vEntPos, vPlyPos, vPlyDir, CULLFRONT, 100000) then
+				if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then
 					e:SetNoDraw(true)
+					if ply:IsLineOfSightClear(e:GetPos()) then					
+						e:SetNoDraw(false)
+					end
 					if e:IsPlayer() then
 						e:SetNoDraw(false)						
-					end	
-					if e:GetClass() == "viewmodel" and "player" and "weapon" and "info_node" then
-						e:SetNoDraw(false)
 					end
-					if ply:IsLineOfSightClear(vEntPos) then					
-						e:SetNoDraw(false)
-					end
+				end
+				if e:GetClass() == "viewmodel" and "player" and "weapon" and "info_node" then
+					e:SetNoDraw(false)
 				end
 			end
 		end
 
-		coroutine.create(main)
+		timer.Create("pootis", 0.2, 0, main)
 
-		timer.Create("loop", 0.2, 0, main)
-		
 	end -- End of if
 
 end -- End of runonce
