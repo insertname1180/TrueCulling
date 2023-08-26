f26a2021c5a07fbcf7c3fa6ab068b460e0a71565
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-26 00:44:40 -0400

    Rework code, fixed some stuttering

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -1,35 +1,39 @@
-function runonce()
+jit.on()
 
-	if CLIENT then
+CULLFRONT = math.sin(0.42261826) -- 150 Degrees forward
 
-		CULLFRONT = math.sin(0.42261826) -- 150 Degrees forward		
+function init()
+	if engine.TickCount()%1 == 0 then
 		ply = LocalPlayer()
-		jit.on()
-
 		hook.Remove("InitPostEntity", "runonce")
-
-		function init()
-			tAllEnts = ents.GetAll()
-			for a, e in pairs(tAllEnts) do
-				e:SetNoDraw(true)	
+		tAllEnts = ents.GetAll()
+		for a, e in pairs(tAllEnts) do
+			e:SetNoDraw(true)
+			if engine.TickCount()%33 == 0 then
 				if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then
-					if ply:IsLineOfSightClear(e:GetPos()) then					
-						e:SetNoDraw(false)
+					if engine.TickCount()%1 == 0 then
+						if ply:IsLineOfSightClear(e:GetPos()) then					
+							e:SetNoDraw(false)
+						end
+					else
+						return
 					end
 				end
+			end
+			if engine.TickCount()%33 == 0 then
 				if e:IsPlayer() or e:GetClass() == "viewmodel" then					
 					e:SetNoDraw(false)
 				end
+			else
+				return
 			end
-			timer.Simple(0.5, init)
 		end
-
-		coroutine.create(init)
-
-		init()
-
+	else
+		return
 	end
-
 end
 
-hook.Add("InitPostEntity", "runonce", runonce)
+coroutine.create(init)
+
+hook.Add("Think", "tick", init)
+hook.Add("InitPostEntity", "runonce", init)
