ec339397b3b493b14aa6f563a9d4379d4a751385
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-26 14:18:47 -0400

    Merge some code supplied from DeltaNeverUsed edited by myself

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -3,34 +3,34 @@ jit.on()
 CULLFRONT = math.sin(0.42261826) -- 150 Degrees forward
 
 function init()
-	if engine.TickCount()%1 == 0 then
-		ply = LocalPlayer()
-		hook.Remove("InitPostEntity", "runonce")
-		tAllEnts = ents.GetAll()
-		for a, e in pairs(tAllEnts) do
-			e:SetNoDraw(true)
-			if engine.TickCount()%33 == 0 then
-				if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then
-					if engine.TickCount()%1 == 0 then
-						if ply:IsLineOfSightClear(e:GetPos()) then					
-							e:SetNoDraw(false)
-						end
-					else
-						return
-					end
-				end
-			end
-			if engine.TickCount()%33 == 0 then
-				if e:IsPlayer() or e:GetClass() == "viewmodel" then					
-					e:SetNoDraw(false)
-				end
-			else
-				return
-			end
-		end
-	else
-		return
-	end
+    hook.Remove("InitPostEntity", "runonce")
+    ply = LocalPlayer()
+    entList = ents.GetAll()
+    for a, e in ipairs(entList) do
+        if engine.TickCount()%2 == 0 then
+            if e:IsValid() then
+                if engine.TickCount()%33 == 0 then
+                    if e:IsPlayer() or e:GetClass() == "viewmodel" then
+                        e:SetNoDraw(false)
+                    else
+                        if engine.TickCount()%33 == 0 then
+                            if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) or ply:IsLineOfSightClear(e:GetPos()) then
+                                e:SetNoDraw(false)
+                            else
+                                e:SetNoDraw(true)
+                            end
+                        else
+                            return
+                        end
+                    end
+                else
+                    return
+                end
+            end
+        else
+            return
+        end
+    end
 end
 
 coroutine.create(init)
