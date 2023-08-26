022e919cad1b4cf0d66387c21de42efc3ceacd82
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-26 14:47:36 -0400

    Rework code and fix some bugs

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -3,34 +3,35 @@ jit.on()
 CULLFRONT = math.sin(0.42261826) -- 150 Degrees forward
 
 function init()
-    hook.Remove("InitPostEntity", "runonce")
-    ply = LocalPlayer()
-    entList = ents.GetAll()
-    for a, e in ipairs(entList) do
-        if engine.TickCount()%2 == 0 then
-            if e:IsValid() then
-                if engine.TickCount()%33 == 0 then
-                    if e:IsPlayer() or e:GetClass() == "viewmodel" then
-                        e:SetNoDraw(false)
-                    else
-                        if engine.TickCount()%33 == 0 then
-                            if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) or ply:IsLineOfSightClear(e:GetPos()) then
-                                e:SetNoDraw(false)
-                            else
-                                e:SetNoDraw(true)
-                            end
-                        else
-                            return
-                        end
-                    end
-                else
-                    return
-                end
-            end
-        else
-            return
-        end
-    end
+	hook.Remove("InitPostEntity", "runonce")
+	hook.Remove("InitPostEntity", "tick")
+	if engine.TickCount()%1 == 0 then
+		ply = LocalPlayer()
+		tAllEnts = ents.GetAll()
+		for a, e in pairs(tAllEnts) do
+			e:SetNoDraw(true)
+			if engine.TickCount()%33 == 0 then
+				if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then
+					if engine.TickCount()%1 == 0 then
+						if ply:IsLineOfSightClear(e:GetPos()) then					
+							e:SetNoDraw(false)
+						end
+					else
+						return
+					end
+				end
+			end
+			if engine.TickCount()%33 == 0 then
+				if e:IsPlayer() or e:GetClass() == "viewmodel" then					
+					e:SetNoDraw(false)
+				end
+			else
+				return
+			end
+		end
+	else
+		return
+	end
 end
 
 coroutine.create(init)
