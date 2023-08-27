6b96bd2e4db621295644d2a80c84917d9bc76a57
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-27 18:26:39 -0400

    mostly fix doors again

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -3,43 +3,48 @@ jit.on()
 CULLFRONT = math.sin(0.42261826) -- 150 Degrees forward
 
 function init()
-	hook.Remove("InitPostEntity", "runonce")
-	ply = LocalPlayer()
-	tAllEnts = ents.GetAll()
-	for a, e in pairs(tAllEnts) do
-		e:SetNoDraw(true)
-		if engine.TickCount()%1 == 0 then
-			if engine.TickCount()%16.5 == 0 then
-				if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then
-					if engine.TickCount()%16.5 == 0 then
-						if ply:IsLineOfSightClear(e:GetPos()) then					
-							e:SetNoDraw(false)
-						end
-					else
-						return
-					end
-					if engine.TickCount()%8 == 0 then
-						if e:GetClass() == "prop_door_rotating" or "func_door" then
-							e:SetNoDraw(false)
-						end
-					else
-						return
-					end
+    hook.Remove("InitPostEntity", "runonce")
+    ply = LocalPlayer()
+    tAllEnts = ents.GetAll()
+    for a, e in pairs(tAllEnts) do
+        e:SetNoDraw(true)
+        if engine.TickCount()%1 == 0 then
+            if engine.TickCount()%1 == 0 then
+				if engine.TickCount()%99 == 0 then
+					if e:GetPos():Distance(ply:GetPos()) < 100 then
+            	       	e:SetNoDraw(false)
+            	    end
+				else
+					return
 				end
-			else
-				return
-			end
-			if engine.TickCount()%45 == 0 then
-				if e:IsPlayer() or e:GetClass() == "viewmodel" then					
-					e:SetNoDraw(false)
-				end
-			else
-				return
-			end
-		else
-			return
-		end
-	end
+                if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then
+                    if engine.TickCount()%33 == 0 then
+                        if ply:IsLineOfSightClear(e:GetPos()) then                    
+                            e:SetNoDraw(false)
+                        end
+                    else
+                        return
+                    end
+                    if e:GetClass() == "prop_door_rotating" or "func_door" then
+                        e:SetNoDraw(false)
+                    else
+                        return
+                    end
+                end
+            else
+                return
+            end
+            if engine.TickCount()%66 == 0 then
+                if e:IsPlayer() or e:GetClass() == "viewmodel" then                    
+                    e:SetNoDraw(false)
+                end
+            else
+                return
+            end
+        else
+            return
+        end
+    end
 end
 
 coroutine.create(init)
