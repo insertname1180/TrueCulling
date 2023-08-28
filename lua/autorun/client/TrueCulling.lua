38f489ac36686895553556da878101ad14b0f2dd
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-27 21:05:01 -0400

    Fix performance issues

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -8,27 +8,24 @@ function init()
     tAllEnts = ents.GetAll()
     for a, e in pairs(tAllEnts) do
         e:SetNoDraw(true)
-        if engine.TickCount()%1 == 0 then
-			if engine.TickCount()%16.5 == 0 then
-				if e:GetPos():Distance(ply:GetPos()) < 75 then
-        	       	e:SetNoDraw(false)
-        	    end
-			else
-				return
-			end
-            if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then
-                if engine.TickCount()%16.5 == 0 then
+        if engine.TickCount()%2 == 0 then
+            if util.IsPointInCone(e:GetPos(), ply:GetPos() * 0.85, ply:GetAimVector(), CULLFRONT, 100000) then
+                if engine.TickCount()%45 == 0 then
                     if ply:IsLineOfSightClear(e:GetPos()) then                    
                         e:SetNoDraw(false)
                     end
                 else
                     return
                 end
-                if e:GetClass() == "prop_door_rotating" or "func_door" then
-                    e:SetNoDraw(false)
-                end
+				if engine.TickCount()%4 == 0 then
+		            if e:GetClass() == "prop_door_rotating" or "func_door" then
+		                e:SetNoDraw(false)
+		            end
+		        else
+	                return
+	            end
             end
-            if engine.TickCount()%16.5 == 0 then
+            if engine.TickCount()%45 == 0 then
                 if e:IsPlayer() or e:GetClass() == "viewmodel" then                    
                     e:SetNoDraw(false)
                 end
