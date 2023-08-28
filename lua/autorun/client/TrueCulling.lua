313a5cc483df729ecd97c327c116d6fb28585eb9
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-27 21:38:12 -0400

    fix doors

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -1,6 +1,6 @@
 jit.on()
 
-CULLFRONT = math.sin(0.42261826) -- 150 Degrees forward
+CULLFRONT = math.sin(0.57357644) -- 150 Degrees forward
 
 function init()
     hook.Remove("InitPostEntity", "runonce")
@@ -9,21 +9,21 @@ function init()
     for a, e in pairs(tAllEnts) do
         e:SetNoDraw(true)
         if engine.TickCount()%2 == 0 then
-            if util.IsPointInCone(e:GetPos(), ply:GetPos() * 0.85, ply:GetAimVector(), CULLFRONT, 100000) then
-                if engine.TickCount()%45 == 0 then
-                    if ply:IsLineOfSightClear(e:GetPos()) then                    
-                        e:SetNoDraw(false)
-                    end
-                else
-                    return
-                end
+            if util.IsPointInCone(e:GetPos() + Vector(0,0,-50), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then
 				if engine.TickCount()%4 == 0 then
 		            if e:GetClass() == "prop_door_rotating" or "func_door" then
 		                e:SetNoDraw(false)
 		            end
 		        else
 	                return
-	            end
+	            end                
+				if engine.TickCount()%45 == 0 then
+                    if ply:IsLineOfSightClear(e:GetPos() + Vector(0,0,-50)) then                    
+                        e:SetNoDraw(false)
+                    end
+                else
+                    return
+                end
             end
             if engine.TickCount()%45 == 0 then
                 if e:IsPlayer() or e:GetClass() == "viewmodel" then                    
