c2a0aa2f33a655c8f2915a826b880ab2661c7342
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-28 17:36:40 -0400

    Fix code

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -12,7 +12,7 @@ function init()
 	if engine.TickCount()%3 == 0 then
 		for a, e in pairs(tAllEnts) do
 	 		e:SetNoDraw(true)
-			if engine.TickCount()%11 == 0 then
+			if engine.TickCount()%22 == 0 then
 				if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then         
 					if ply:IsLineOfSightClear(e:GetPos() + Vector(-25,-25,35)) or ply:IsLineOfSightClear(e:GetPos() + Vector(100,100,-10)) then                    
 						e:SetNoDraw(false)
@@ -28,14 +28,14 @@ function init()
 			else
 				return
 			end
-			if engine.TickCount()%33 == 0 then
+			if engine.TickCount()%22 == 0 then
 				if e:IsPlayer() then                    
 					e:SetNoDraw(false)
 				end
 			else
 				return
 			end
-			if engine.TickCount()%45 == 0 then
+			if engine.TickCount()%22 == 0 then
 				if e:GetClass() == "viewmodel" then
 					e:SetNoDraw(false)
 				end
