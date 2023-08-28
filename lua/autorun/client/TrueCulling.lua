d0ab4539d89d75c157726b3c11aaecd42c717925
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-28 10:58:50 -0400

    Unroll for loop and optimize

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -2,39 +2,29 @@ jit.on()
 
 CULLFRONT = math.sin(0.64278761) -- 150 Degrees forward
 
+num = 1
+
 function init()
     hook.Remove("InitPostEntity", "runonce")
-    ply = LocalPlayer()
-    tAllEnts = ents.GetAll()
-	if engine.TickCount()%3 == 0 then
-		for a, e in pairs(tAllEnts) do
-		    e:SetNoDraw(true)
-			if engine.TickCount()%22.5 == 0 then
-				if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then         
-					if ply:IsLineOfSightClear(e:GetPos() + Vector(-25,-25,35)) or ply:IsLineOfSightClear(e:GetPos() + Vector(100,100,-10)) then                    
-						e:SetNoDraw(false)
-					end
-				end
-			else
-				return
-			end
-			if engine.TickCount()%22.5 == 0 then
-				if e:GetPos():Distance(ply:GetPos()) < 100 then
-					e:SetNoDraw(false)
-			    end
-			else
-		        return
-		    end
-		    if engine.TickCount()%22.5 == 0 then
-		        if e:IsPlayer() or e:GetClass() == "viewmodel" then                    
-		            e:SetNoDraw(false)
-		        end
-		    else
-		        return
-		    end
+	ply = LocalPlayer()		
+	if num >= ents.GetCount() then
+		num = 1
+	end
+	num = num + 1
+	e = Entity(num)
+	if e:IsValid() then
+		e:SetNoDraw(true)
+		if e:IsPlayer() or e:GetClass() == "viewmodel" then
+			e:SetNoDraw(false)
 		end
-	else
-		return
+		if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then         
+			if ply:IsLineOfSightClear(e:GetPos() + Vector(-25,-25,35)) or ply:IsLineOfSightClear(e:GetPos() + Vector(100,100,-10)) then                    
+				e:SetNoDraw(false)
+			end
+		end
+		if e:GetPos():Distance(ply:GetPos()) < 100 then
+			e:SetNoDraw(false)
+		end
 	end
 end
 
