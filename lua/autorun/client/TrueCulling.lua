e4ecaa822373f3cc764a3fb4c7ec7e15c69a8edb
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-27 22:47:30 -0400

    Fix doors again by swaping vectors

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -5,25 +5,24 @@ CULLFRONT = math.sin(0.57357644) -- 150 Degrees forward
 function init()
     hook.Remove("InitPostEntity", "runonce")
     ply = LocalPlayer()
+	plyang = ply:GetAngles()
     tAllEnts = ents.GetAll()
     for a, e in pairs(tAllEnts) do
         e:SetNoDraw(true)
         if engine.TickCount()%2 == 0 then
-            if util.IsPointInCone(e:GetPos() + Vector(0,0,-50), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then
-				if engine.TickCount()%4 == 0 then
-		            if e:GetClass() == "prop_door_rotating" or "func_door" then
-		                e:SetNoDraw(false)
-		            end
-		        else
-	                return
-	            end                
+            if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then         
 				if engine.TickCount()%45 == 0 then
-                    if ply:IsLineOfSightClear(e:GetPos() + Vector(0,0,-50)) then                    
-                        e:SetNoDraw(false)
-                    end
-                else
-                    return
-                end
+					if plyang.y > 0 then
+		                if ply:IsLineOfSightClear(e:GetPos() + Vector(-25,-25,0)) then                    
+		                    e:SetNoDraw(false)                
+						end
+		            end
+					if plyang.y < 180 then
+						if ply:IsLineOfSightClear(e:GetPos() + Vector(25,25,0)) then                    
+		                    e:SetNoDraw(false)                
+						end
+		            end
+				end
             end
             if engine.TickCount()%45 == 0 then
                 if e:IsPlayer() or e:GetClass() == "viewmodel" then                    
