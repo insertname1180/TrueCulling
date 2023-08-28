7a164f52e122a6a565fd9ca9bd0a209a45dbe454
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-28 17:29:48 -0400

    Fix code and % lows

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -1,4 +1,5 @@
 jit.on()
+jit.opt.start(3)
 
 CULLFRONT = math.sin(0.64278761) -- 150 Degrees forward
 
@@ -6,29 +7,47 @@ num = 1
 
 function init()
     hook.Remove("InitPostEntity", "runonce")
-	ply = LocalPlayer()		
-	if num >= ents.GetCount() then
-		num = 1
-	end
-	num = num + 1
-	e = Entity(num)
-	if e:IsValid() then
-		e:SetNoDraw(true)
-		if e:IsPlayer() or e:GetClass() == "viewmodel" then
-			e:SetNoDraw(false)
-		end
-		if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then         
-			if ply:IsLineOfSightClear(e:GetPos() + Vector(-25,-25,35)) or ply:IsLineOfSightClear(e:GetPos() + Vector(100,100,-10)) then                    
-				e:SetNoDraw(false)
+    ply = LocalPlayer()
+	tAllEnts = ents.GetAll()
+	if engine.TickCount()%3 == 0 then
+		for a, e in pairs(tAllEnts) do
+	 		e:SetNoDraw(true)
+			if engine.TickCount()%11 == 0 then
+				if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then         
+					if ply:IsLineOfSightClear(e:GetPos() + Vector(-25,-25,35)) or ply:IsLineOfSightClear(e:GetPos() + Vector(100,100,-10)) then                    
+						e:SetNoDraw(false)
+					end
+				end
+			else
+				return
+			end
+			if engine.TickCount()%22 == 0 then
+				if e:GetPos():Distance(ply:GetPos()) < 100 then
+					e:SetNoDraw(false)
+				end
+			else
+				return
+			end
+			if engine.TickCount()%33 == 0 then
+				if e:IsPlayer() then                    
+					e:SetNoDraw(false)
+				end
+			else
+				return
+			end
+			if engine.TickCount()%45 == 0 then
+				if e:GetClass() == "viewmodel" then
+					e:SetNoDraw(false)
+				end
+			else
+				return
 			end
 		end
-		if e:GetPos():Distance(ply:GetPos()) < 100 then
-			e:SetNoDraw(false)
-		end
+	else
+		return
 	end
+	return
 end
-
-coroutine.create(init)
 
 hook.Add("Think", "tick", init)
 hook.Add("InitPostEntity", "runonce", init)
