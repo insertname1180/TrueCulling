922bc131fa7f9ec57a07650872143f4ce7e9cf6c
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-28 00:46:57 -0400

    fix door bugs

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -5,31 +5,28 @@ CULLFRONT = math.sin(0.57357644) -- 150 Degrees forward
 function init()
     hook.Remove("InitPostEntity", "runonce")
     ply = LocalPlayer()
-	plyang = ply:GetAngles()
     tAllEnts = ents.GetAll()
     for a, e in pairs(tAllEnts) do
         e:SetNoDraw(true)
-        if engine.TickCount()%2 == 0 then
-            if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then         
-				if engine.TickCount()%45 == 0 then
-					if plyang.y > 0 then
-		                if ply:IsLineOfSightClear(e:GetPos() + Vector(-25,-25,0)) then                    
-		                    e:SetNoDraw(false)                
-						end
-		            end
-					if plyang.y < 180 then
-						if ply:IsLineOfSightClear(e:GetPos() + Vector(25,25,0)) then                    
-		                    e:SetNoDraw(false)                
-						end
-		            end
+		if engine.TickCount()%22.5 == 0 then
+		    if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then         
+				if ply:IsLineOfSightClear(e:GetPos() + Vector(-25,-25,0)) or ply:IsLineOfSightClear(e:GetPos() + Vector(25,25,0)) then                    
+				    e:SetNoDraw(false)
 				end
-            end
-            if engine.TickCount()%45 == 0 then
-                if e:IsPlayer() or e:GetClass() == "viewmodel" then                    
-                    e:SetNoDraw(false)
-                end
-            else
-                return
+			end
+		else
+			return
+		end
+		if engine.TickCount()%22.5 == 0 then
+			if e:GetPos():Distance(ply:GetPos()) < 75 then
+				e:SetNoDraw(false)
+	        end
+		else
+            return
+        end
+        if engine.TickCount()%22.5 == 0 then
+            if e:IsPlayer() or e:GetClass() == "viewmodel" then                    
+                e:SetNoDraw(false)
             end
         else
             return
