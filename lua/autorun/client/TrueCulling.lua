510bafca318232fbe04130c81af4089981c3b181
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-29 17:33:04 -0400

    Completely rework TrueCulling.lua
    MK1 Update

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -1,65 +1,38 @@
 jit.on()
 jit.opt.start(3)
 
-CULLFRONT = math.sin(0.64278761) -- 150 Degrees forward
+CULLFRONT = math.sin(0.64278761) -- Degrees forward angle in sine
+num = 0
+ent_ = 0
 
-function init()
-    hook.Remove("InitPostEntity", "runonce")
-    ply = LocalPlayer()
-    tAllEnts = ents.GetAll()
-    if engine.TickCount()%16.5 == 0 then
-        for a, e in pairs(tAllEnts) do
-             e:SetNoDraw(true)
-            if engine.TickCount()%24 == 0 then
-                if util.IsPointInCone(e:GetPos(), ply:GetPos(), ply:GetAimVector(), CULLFRONT, 100000) then
-                    if engine.TickCount()%10 == 0 then       
-                        if ply:IsLineOfSightClear(e:GetPos() + Vector(-25,-25,35)) or ply:IsLineOfSightClear(e:GetPos() + Vector(100,100,-10)) then                    
-                            e:SetNoDraw(false)
-                        else
-                            return
-                        end
-                    else
-                        return
-                    end
-                else
-                    return
-                end
-            else
-                return
-            end
-            if engine.TickCount()%45 == 0 then
-                if e:GetPos():Distance(ply:GetPos()) < 100 then
-                    e:SetNoDraw(false)
-                else
-                    return
-                end
-            else
-                return
-            end
-            if engine.TickCount()%33 == 6 then
-                if e:GetClass() == "player" then                    
-                    e:SetNoDraw(false)
-                else
-                    return
-                end
-            else
-                return
-            end
-            if engine.TickCount()%33 == 6 then
-                if e:GetClass() == "viewmodel" then
-                    e:SetNoDraw(false)
-                else
-                    return
-                end
-            else
-                return
-            end
-        end
-    else
-        return
-    end
-    return
+function main()
+	loop = true
+	while (loop) do
+		if ent_ >= ents.GetCount() * 2 then			
+			ent_ = 0
+		end
+		ent_ = ent_ + 1
+		if Entity(ent_):IsValid() then
+			ents_ = Entity(ent_)
+			ents_:SetNoDraw(true)
+			if util.IsPointInCone(ents_:GetPos(), LocalPlayer():GetPos(), LocalPlayer():GetAimVector(), CULLFRONT, 100000) then       
+				if LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(-25,-25,35)) or LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(100,100,-10)) then                    
+					ents_:SetNoDraw(false)
+				end
+			end
+			if ents_:GetPos():Distance(LocalPlayer():GetPos()) < 100 then
+				ents_:SetNoDraw(false)
+			end
+			if ents_:IsPlayer() or ents_:GetClass() == "viewmodel" then                    
+				ents_:SetNoDraw(false)
+			end
+		end
+		if ent_ >= ents.GetCount() * 2 then
+			break 
+		end
+	end
+	timer.Simple(0.1, main)
 end
 
-hook.Add("Think", "tick", init)
-hook.Add("InitPostEntity", "runonce", init)
+main()
+hook.Add("InitPostEntity", "initents", main)
