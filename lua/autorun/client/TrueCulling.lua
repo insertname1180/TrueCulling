d85b108cfc2f653b3fd950d9ef7a82c79645c3ff
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-29 19:02:31 -0400

    fix major stuttering

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -1,37 +1,50 @@
 jit.on()
-jit.opt.start(3)
 
-CULLFRONT = math.sin(0.64278761) -- Degrees forward angle in sine
-num = 0
+CULLFRONT = math.sin(0.57357644) -- Degrees forward angle in sine
 ent_ = 0
+occur = 0
+occur_ = 0
+occur__ = 0
+tl = occur - CurTime()
+tl_ = occur_ - CurTime()
+tl__ = occur_ - CurTime()
 
 function main()
-	loop = true
-	while (loop) do
-		if ent_ >= ents.GetCount() * 2 then			
-			ent_ = 0
-		end
-		ent_ = ent_ + 1
-		if Entity(ent_):IsValid() then
-			ents_ = Entity(ent_)
-			ents_:SetNoDraw(true)
-			if util.IsPointInCone(ents_:GetPos(), LocalPlayer():GetPos(), LocalPlayer():GetAimVector(), CULLFRONT, 100000) then       
-				if LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(-25,-25,35)) or LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(100,100,-10)) then                    
-					ents_:SetNoDraw(false)
+	if tl < 0 then
+		loop = true
+		while (loop) do
+			if tl__ < 0 then			
+				if ent_ >= ents.GetCount() * 2 then			
+					ent_ = 0
 				end
+				ent_ = ent_ + 1
+				occur__ = CurTime() + 0.75
 			end
-			if ents_:GetPos():Distance(LocalPlayer():GetPos()) < 100 then
-				ents_:SetNoDraw(false)
+			if tl_ < 0 then
+				if Entity(ent_):IsValid() then
+					ents_ = Entity(ent_)
+					ents_:SetNoDraw(true)
+					if util.IsPointInCone(ents_:GetPos(), LocalPlayer():GetPos(), LocalPlayer():GetAimVector(), CULLFRONT, 100000) then
+						if LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(-25,-25,35)) or LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(100,100,-10)) then                    
+							ents_:SetNoDraw(false)
+						end
+					end
+					if ents_:IsPlayer() or ents_:GetClass() == "viewmodel" then                    
+						ents_:SetNoDraw(false)
+					end
+					if ents_:GetPos():Distance(LocalPlayer():GetPos()) < 100 then
+						ents_:SetNoDraw(false)
+					end
+				end
+				occur_ = CurTime() + 0.75
 			end
-			if ents_:IsPlayer() or ents_:GetClass() == "viewmodel" then                    
-				ents_:SetNoDraw(false)
+			occur = CurTime() + 0.75
+			if ent_ >= ents.GetCount() * 2 then
+				timer.Simple(0.2, main)
+				break
 			end
-		end
-		if ent_ >= ents.GetCount() * 2 then
-			break 
 		end
 	end
-	timer.Simple(0.1, main)
 end
 
 main()
