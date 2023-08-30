496127ffdc71bf1cc6ecdc81a70401899f76d9cf
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-29 22:44:56 -0400

    Change back to for loop

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -2,15 +2,13 @@ jit.on()
 
 CULLFRONT = math.sin(0.57357644) -- Degrees forward angle in sine
 
-function main()		
-	ent_ = 0
-	while (ent_ ~= ents.GetCount() * 2) do
-		ent_ = ent_ + 1
-		if Entity(ent_):IsValid() then
-			ents_ = Entity(ent_)
-			if engine.TickCount()%45 == 0 then
-				ents_:SetNoDraw(true)
-			end
+function main()
+	allEnts = ents.GetAll()
+	for a, ents_ in pairs(allEnts) do
+		if engine.TickCount()%33 == 0 then
+			ents_:SetNoDraw(true)
+		end
+		if engine.TickCount()%16.5 == 0 then
 			if ents_:GetNoDraw(true) then
 				if util.IsPointInCone(ents_:GetPos(), LocalPlayer():GetPos(), LocalPlayer():GetAimVector(), CULLFRONT, 100000) then
 					if LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(-25,-25,35)) or LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(100,100,-10)) then                    
@@ -18,12 +16,14 @@ function main()
 					end
 				end
 			end
-			if ents_:IsPlayer() or ents_:GetClass() == "viewmodel" then                    
-				ents_:SetNoDraw(false)
-			end
-			if ents_:GetPos():Distance(LocalPlayer():GetPos()) < 100 then
-				ents_:SetNoDraw(false)
-			end
+		else
+			return
+		end
+		if ents_:IsPlayer() or ents_:GetClass() == "viewmodel" then                    
+			ents_:SetNoDraw(false)
+		end
+		if ents_:GetPos():Distance(LocalPlayer():GetPos()) < 100 then
+			ents_:SetNoDraw(false)
 		end
 	end
 end
