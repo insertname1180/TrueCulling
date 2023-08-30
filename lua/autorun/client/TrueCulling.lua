8e551592ae50cdca8a700723b726f9d2218c369d
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-30 13:17:42 -0400

    Update TrueCulling.lua

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -4,19 +4,17 @@ CULLFRONT = math.sin(0.57357644) -- Degrees forward angle in sine
 
 function main()
 	allEnts = ents.GetAll()
-	if engine.TickCount()%6 == 0 then
+	if engine.TickCount()%4 == 0 then
 		for a, ents_ in pairs(allEnts) do
-			if engine.TickCount()%16 == 0 then
+			if engine.TickCount()%10 == 0 then
 				ents_:SetNoDraw(true)
 			end
-			if engine.TickCount()%22 == 0 then
+			if engine.TickCount()%10 == 0 then
 				if ents_:GetNoDraw(true) then
 					if util.IsPointInCone(ents_:GetPos(), LocalPlayer():GetPos(), LocalPlayer():GetAimVector(), CULLFRONT, 100000) then
-						if engine.TickCount()%16.5 == 0 then
-							if LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(-25,-25,35)) or LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(100,100,-10)) then
-								if ents_:GetNoDraw(true) then 
-									ents_:SetNoDraw(false)
-								end
+						if LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(-25,-25,35)) or LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(100,100,-10)) then
+							if ents_:GetNoDraw(true) then 
+								ents_:SetNoDraw(false)
 							end
 						end
 					end
@@ -24,7 +22,7 @@ function main()
 			else
 				return
 			end
-			if engine.TickCount()%99 == 0 then
+			if engine.TickCount()%10 == 0 then
 				if ents_:IsPlayer() or ents_:GetClass() == "viewmodel" then    
 					if ents_:GetNoDraw(true) then                
 						ents_:SetNoDraw(false)
@@ -33,7 +31,7 @@ function main()
 			else
 				return
 			end
-			if engine.TickCount()%120 == 0 then
+			if engine.TickCount()%10 == 0 then
 				if ents_:GetPos():Distance(LocalPlayer():GetPos()) < 100 then
 					if ents_:GetNoDraw(true) then				
 						ents_:SetNoDraw(false)
