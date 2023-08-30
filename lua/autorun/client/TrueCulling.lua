44b85214e89a4ddd4863b1bc6c87f12d71e71ee7
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2023-08-30 03:08:02 -0400

    safeguards and less checks

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -4,27 +4,49 @@ CULLFRONT = math.sin(0.57357644) -- Degrees forward angle in sine
 
 function main()
 	allEnts = ents.GetAll()
-	for a, ents_ in pairs(allEnts) do
-		if engine.TickCount()%33 == 0 then
-			ents_:SetNoDraw(true)
-		end
-		if engine.TickCount()%16.5 == 0 then
-			if ents_:GetNoDraw(true) then
-				if util.IsPointInCone(ents_:GetPos(), LocalPlayer():GetPos(), LocalPlayer():GetAimVector(), CULLFRONT, 100000) then
-					if LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(-25,-25,35)) or LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(100,100,-10)) then                    
+	if engine.TickCount()%6 == 0 then
+		for a, ents_ in pairs(allEnts) do
+			if engine.TickCount()%16 == 0 then
+				if ents_:GetNoDraw(false) then
+					ents_:SetNoDraw(true)
+				end
+			end
+			if engine.TickCount()%22 == 0 then
+				if ents_:GetNoDraw(true) then
+					if util.IsPointInCone(ents_:GetPos(), LocalPlayer():GetPos(), LocalPlayer():GetAimVector(), CULLFRONT, 100000) then
+						if engine.TickCount()%16.5 == 0 then
+							if LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(-25,-25,35)) or LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(100,100,-10)) then
+								if ents_:GetNoDraw(true) then 
+									ents_:SetNoDraw(false)
+								end
+							end
+						end
+					end
+				end
+			else
+				return
+			end
+			if engine.TickCount()%99 == 0 then
+				if ents_:IsPlayer() or ents_:GetClass() == "viewmodel" then    
+					if ents_:GetNoDraw(true) then                
 						ents_:SetNoDraw(false)
 					end
 				end
+			else
+				return
 			end
-		else
-			return
+			if engine.TickCount()%120 == 0 then
+				if ents_:GetPos():Distance(LocalPlayer():GetPos()) < 100 then
+					if ents_:GetNoDraw(true) then				
+						ents_:SetNoDraw(false)
+					end			
+				end
+			else
+				return
+			end
 		end
-		if ents_:IsPlayer() or ents_:GetClass() == "viewmodel" then                    
-			ents_:SetNoDraw(false)
-		end
-		if ents_:GetPos():Distance(LocalPlayer():GetPos()) < 100 then
-			ents_:SetNoDraw(false)
-		end
+	else
+		return
 	end
 end
 
