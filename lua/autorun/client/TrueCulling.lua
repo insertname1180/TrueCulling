4b2a139d57fd14d8bb928b1bc39d6c29ea8c7e4e
shiiru <shiirumain@gmail.com>
2023-09-01 22:14:50 +0200

    probably don't need it, right?????

--- lua/autorun/client/TrueCulling.lua
+++ lua/autorun/client/TrueCulling.lua
@@ -8,18 +8,16 @@ function main()
 		for a, ents_ in pairs(allEnts) do
 			if engine.TickCount()%10 == 0 then
 				ents_:SetNoDraw(true)
-				if ents_:GetNoDraw() then
-					if util.IsPointInCone(ents_:GetPos(), LocalPlayer():GetPos(), LocalPlayer():GetAimVector(), CULLFRONT, 100000) then
-						if LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(-25,-25,-25)) or LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(10,10,10)) then
-							ents_:SetNoDraw(false)
-						end
-					end
-					if ents_:IsPlayer() or ents_:GetClass() == "viewmodel" then       
+				if util.IsPointInCone(ents_:GetPos(), LocalPlayer():GetPos(), LocalPlayer():GetAimVector(), CULLFRONT, 100000) then
+					if LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(-25,-25,-25)) or LocalPlayer():IsLineOfSightClear(ents_:GetPos() + Vector(10,10,10)) then
 						ents_:SetNoDraw(false)
 					end
-					if ents_:GetPos():Distance(LocalPlayer():GetPos()) < 100 then			
-						ents_:SetNoDraw(false)		
-					end
+				end
+				if ents_:IsPlayer() or ents_:GetClass() == "viewmodel" then       
+					ents_:SetNoDraw(false)
+				end
+				if ents_:GetPos():Distance(LocalPlayer():GetPos()) < 100 then			
+					ents_:SetNoDraw(false)		
 				end
 				if ents_:GetClass() == "10C_BaseFlex" then
 					ents_:SetNoDraw(true)
